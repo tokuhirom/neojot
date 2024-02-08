31c275f641858ff6e491fc19eaf4b3bf334758bd
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-02-08 12:09:35 +0900

    cleanup

--- src/lib/FileCardItem.svelte
+++ src/lib/FileCardItem.svelte
@@ -18,33 +18,3 @@
           title={file.title.replace(/TODO: /, "☐️").replace(/DONE: /, "☑")}
           content={file.content.split('\n').slice(1).join('\n')}
 />
-
-<style>
-    .card {
-        display: flex; /* Flexboxを有効にします */
-        flex-direction: column; /* アイテムを縦方向に並べます */
-        align-items: flex-start; /* 左寄せにします（テキストが左揃えの場合） */
-        justify-content: flex-start; /* アイテムを上に寄せます */
-        float: left;
-        width: 100px;
-        margin: 9px;
-        background-color: #f6f6f6;
-        color: #0f0f0f;
-        padding: 9px;
-        height: 120px;
-        overflow-y: hidden;;
-        border-radius: 2px;
-        text-align: left;
-        vertical-align: top;
-    }
-
-    .title {
-        font-size: 15px;
-        font-weight: bold;
-        word-break: break-all;
-    }
-
-    .content {
-        font-size: 10px;
-    }
-</style>
