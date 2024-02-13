6e54b5feeaba568c046d092eeff9d0ae470d7482
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-02-13 16:16:55 +0900

    suppress warnings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import './styles.css';
 import App from './App.svelte';
 
+const target = document.getElementById('app');
+if (!target) {
+    alert('Missing target element');
+}
 const app = new App({
-    target: document.getElementById('app'),
+    target: target!,
 });
 
 export default app;
