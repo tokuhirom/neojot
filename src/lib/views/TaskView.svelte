8598c3ec6db49705f70212e0fad2acaa5faf6347
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-02-21 14:43:18 +0900

    greate new task management

--- src/lib/views/TaskView.svelte
+++ src/lib/views/TaskView.svelte
@@ -9,11 +9,60 @@
     export let selectedItem: FileItem | undefined = undefined;
     export let onSelectItem: (FileItem) => void;
     let filteredFileItems: FileItem[] = [];
+    let tasks: Task[] = [];
+
+    export type Task = {
+        date: string;
+        symbol: string;
+        duration: number;
+        title: string;
+        fileItem: FileItem;
+    };
+    function extractTasks(fileItems: FileItem[]): Task[] {
+        const tasks: Task[] = [];
+        const regex = /^\[(\d{4}-\d\d-\d\d)]([!.])(\d*)\s+(.+)/;
+
+        fileItems.forEach((fileItem) => {
+            fileItem.content.split('\n').forEach((line) => {
+                const match = line.match(regex);
+                if (match) {
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    const [_p, date, symbol, duration, title] = match;
+                    tasks.push({
+                        date,
+                        symbol,
+                        duration: parseInt(duration) || 0,
+                        title,
+                        fileItem,
+                    });
+                }
+            });
+        });
+
+        return tasks;
+    }
+
+    function sortTasks(tasks: Task[]): Task[] {
+        // 未完了タスクと完了済みタスクに分ける
+        const incompleteTasks = tasks.filter((task) => task.symbol !== '.');
+        const completedTasks = tasks.filter((task) => task.symbol === '.');
+
+        // 未完了タスクを日付で昇順にソート
+        incompleteTasks.sort((a, b) => {
+            if (a.date < b.date) return -1;
+            if (a.date > b.date) return 1;
+            return 0;
+        });
+
+        // 完了済みタスクはそのまま末尾に追加
+        return [...incompleteTasks, ...completedTasks];
+    }
 
     onMount(async () => {
         filteredFileItems = dataFileItems.filter((it) =>
             it.title.includes('TODO:'),
         );
+        tasks = sortTasks(extractTasks(dataFileItems));
         selectedItem = filteredFileItems[0];
     });
 
@@ -28,14 +77,31 @@
         allFileItems = allFileItems;
         onSelectItem(fileItem);
     }
+
+    function determineClass(task) {
+        const today = new Date().toISOString().split('T')[0];
+        if (task.symbol === '.') {
+            return 'done';
+        } else if (task.symbol === '!' && task.date === today) {
+            return 'today';
+        } else if (task.symbol === '!' && task.date < today) {
+            return 'overdue';
+        }
+        return '';
+    }
 </script>
 
 <div class="task-view">
     <div class="file-list">
-        {#each filteredFileItems as fileItem}
-            <button on:click={() => onSelectItem(fileItem)}
-                >{fileItem.title}</button
+        {#each tasks as task}
+            <button
+                on:click={() => onSelectItem(task.fileItem)}
+                class={determineClass(task)}
             >
+                <span class="date">{task.date}{task.symbol}</span>
+                <span class="title">{task.title}</span>
+                <span class="file-title">{task.fileItem.title}</span>
+            </button>
         {/each}
     </div>
     <div class="log-view">
@@ -96,5 +162,35 @@
         flex-grow: 1;
         margin-block-start: 0;
         margin-block-end: 0;
+    }
+
+    .date {
+        display: block;
+    }
+    .title {
+        display: block;
+    }
+    .file-title {
+        display: block;
+        font-size: 80%;
+    }
+
+    /* 完了済みタスク */
+    .done {
+        color: #757575;
+        text-decoration: line-through;
+        background-color: #f0f0f0;
+    }
+
+    /* 今日が期限のタスク */
+    .today {
+        color: #007bff !important;
+        background-color: #007bff; /* 明るい青、Bootstrapのプライマリーカラーを参考 */
+        border-color: #007bff;
+    }
+
+    /* 期限切れのタスク */
+    .overdue {
+        color: #dc3545 !important;
     }
 </style>
