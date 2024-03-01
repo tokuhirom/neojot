8a63d8bce2b609c7f85952afcfee58f1f7dc4cb5
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-03-02 00:48:40 +0900

    Open URL by Cmd-b

--- src/lib/markdown/EntryView.svelte
+++ src/lib/markdown/EntryView.svelte
@@ -172,7 +172,7 @@
             );
         }
 
-        function openInternalLink(view: EditorView) {
+        async function openInternalLink(view: EditorView) {
             const { from, to } = view.state.selection.main;
             if (from === to) {
                 // カーソル位置のみをチェック
@@ -181,16 +181,24 @@
                 const lineOffset = from - line.from; // 行内オフセットを計算
 
                 // カーソル位置から内部リンクを探す
-                const linkRegex = /\[\[.*?]]/g;
+                const linkRegex = /\[\[.*?]]|https?:\/\/\S+/g;
                 let match: RegExpExecArray | null;
                 while ((match = linkRegex.exec(lineText)) !== null) {
                     if (
                         match.index <= lineOffset &&
                         match.index + match[0].length >= lineOffset
                     ) {
-                        const pageName = match[0].slice(2, -2); // リンク名の取得
-                        findOrCreateEntry(pageName);
-                        return true;
+                        if (match[0].startsWith('http')) {
+                            const url = match[0];
+                            console.log(`opening url: ${url}`);
+                            await invoke('open_url', { url });
+                            return true;
+                        } else {
+                            // 内部リンクの場合はページを開く
+                            const pageName = match[0].slice(2, -2); // リンク名の取得
+                            findOrCreateEntry(pageName);
+                            return true;
+                        }
                     }
                 }
             }
