8972d2fd7554e2a3f9de779df3d0a8252fae5a14
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-03-20 07:32:41 +0900

    use BasicClearableSearchBox on ClearableSearchBox

--- src/lib/search/ClearableSearchBox.svelte
+++ src/lib/search/ClearableSearchBox.svelte
@@ -8,6 +8,7 @@
     import { makeMigemoRegexes } from './Migemo';
     import { type FileItem } from '../file_item/FileItem.ts';
     import { searchFileItems, type SearchResult } from '../file_item/Search';
+    import BasicClearableSearchBox from './BasicClearableSearchBox.svelte';
 
     let migemoRegexes: RegExp[] | undefined = undefined;
     searchKeywordStore.subscribe(async (value) => {
@@ -41,34 +42,4 @@
     }
 </script>
 
-<div class="clearable-search-box">
-    <input type="text" bind:value={$searchKeywordStore} class="search-input" />
-    {#if $searchKeywordStore}
-        <button class="clear-btn" on:click={() => ($searchKeywordStore = '')}
-            >✗</button
-        >
-    {/if}
-</div>
-
-<style>
-    .clearable-search-box {
-        position: relative;
-        display: inline-block;
-        width: 100%;
-        margin-top: 9px;
-        margin-bottom: 9px;
-    }
-    .search-input {
-        font-size: 140%;
-        width: 100%;
-    }
-    .clear-btn {
-        position: absolute;
-        right: 5px;
-        top: 50%;
-        transform: translateY(-50%);
-        border: none;
-        background-color: transparent;
-        cursor: pointer;
-    }
-</style>
+<BasicClearableSearchBox bind:keyword={$searchKeywordStore} />
