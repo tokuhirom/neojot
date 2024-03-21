d2299498b009411d00ca6fb9c60081e1f1972a67
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-03-22 06:55:33 +0900

    cleanup get_title_markdown

--- src-tauri/src/file_item.rs
+++ src-tauri/src/file_item.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 
@@ -24,38 +25,41 @@ pub fn get_title(path: PathBuf, content: &str) -> String {
         } else {
             let basename = basename.trim_end_matches(".excalidraw");
             format!("Draw {}", basename)
-        }
+        };
     }
 
     return get_title_markdown(content);
 }
 
 pub fn get_title_markdown(content: &str) -> String {
-    let re = Regex::new(r"^#+ *([^\n ]+)").unwrap(); // cache?
-
-    return if let Some(captures) = re.captures(content) {
-        println!("GO: {:?}", captures.get(1));
-        captures.get(1).map_or_else(|| "", |m| m.as_str()).to_string()
-    } else {
-        // タイトルが見つからない場合は、最初の空白行でない行を返す。
-        // ただし、TODO 記法の場合は TODO 記法の部分を削除して返す。
-        // /^(TODO|DOING|PLAN|DONE|CANCELED)\[.*]:\s*/ にマッチした場合マッチした部分を消す。
-        let lines = content.lines().clone().skip(1);
-        for line in lines {
-            if line.is_empty() {
-                continue;
-            }
-
-            log::info!("title?: {}", line);
-            return if line.starts_with("TODO[") || line.starts_with("DOING[") || line.starts_with("PLAN[") || line.starts_with("DONE[") || line.starts_with("CANCELED[") {
-                let re = Regex::new(r"^(TODO|DOING|PLAN|DONE|CANCELED)\[.*\]:\s*").unwrap();
-                re.replace(line, "").to_string()
-            } else {
-                line.to_string()
+    if let Some(first_line) = content.lines().next() {
+        if let Some(stripped) = first_line.strip_prefix("# ") {
+            let title = stripped.trim();
+            // タイトルが空白文字でないことを確認
+            if !title.is_empty() {
+                return title.to_owned();
             }
         }
-        return String::from("NO TITLE");
-    };
+    }
+
+    // Markdownタイトルがない場合は、TODO記法をチェック
+    let todo_regex = Regex::new(r"^(TODO|DOING|PLAN|DONE|CANCELED)\[.*\]:\s*").unwrap();
+    for line in content.lines().skip(1) {
+        if line.is_empty() {
+            continue;
+        }
+
+        // TODO記法があればそれを取り除く
+        if todo_regex.is_match(line) {
+            return todo_regex.replace(line, "").to_string().trim().to_owned();
+        }
+
+        // TODO記法がなければその行をそのまま返す
+        return line.trim().to_owned();
+    }
+
+    // タイトルが見つからない場合
+    "NO TITLE".to_owned()
 }
 
 #[cfg(test)]
@@ -65,6 +69,7 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(get_title_markdown("# hello"), String::from("hello"));
+        assert_eq!(get_title_markdown("# hello world"), String::from("hello world"));
         assert_eq!(get_title_markdown("# \nhello"), String::from("hello"));
         assert_eq!(get_title_markdown("# \nTODO[Scheduled: 2024-01-03(Mon)]: hello"), String::from("hello"));
     }
