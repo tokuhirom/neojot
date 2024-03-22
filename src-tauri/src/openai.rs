a747e0dafed360a93ff41c9ab5bfbdce70437a1b
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-03-23 08:03:26 +0900

    remove useless test

--- src-tauri/src/openai.rs
+++ src-tauri/src/openai.rs
@@ -82,23 +82,3 @@ async fn listen_for_tokens(uuid: String, mut chat_stream: Receiver<ChatCompletio
     }
     merged.unwrap().into()
 }
-
-#[cfg(test)]
-mod tests {
-    use std::env;
-
-    use crate::openai::ask_openai;
-
-    #[test]
-    fn it_works() {
-        let openai_token = env::var("OPENAI_API_KEY").unwrap().to_string();
-        ask_openai(
-            "HELLO".to_string(),
-            openai_token,
-            "Here's a markdown notes.\nCould you recommend better title?".to_string(),
-           "# hello\n\nBitcoin is a decentralized digital currency, without a central bank or single administrator, that can be sent from user to user on the peer-to-peer bitcoin network without the need for intermediaries. Transactions are verified by network nodes through cryptography and recorded in a public distributed ledger called a blockchain. Bitcoin was invented in 2008 by an unknown person or group of people using the name Satoshi Nakamoto. The currency began use in 2009 when its implementation was released as open-source software.".to_string()
-        ).unwrap();
-        // assert_eq!(ask_openai("# hello".to_string()), String::from("hello"));
-    }
-}
-
